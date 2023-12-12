9be9702bdf7542a6e4587f93c60ce6717346e0e5
lorenzo <76961588+lolloStorto@users.noreply.github.com>
2023-12-12 09:14:37 +0100

    Update JPanelWithBackground.java

--- src/main/java/Helper/JPanelWithBackground.java
+++ src/main/java/Helper/JPanelWithBackground.java
@@ -10,9 +10,6 @@ public class JPanelWithBackground extends JPanel {
 
     private Image backgroundImage;
 
-    // Some code to initialize the background image.
-    // Here, we use the constructor to load the image. This
-    // can vary depending on the use case of the panel.
     public JPanelWithBackground(String fileName) throws IOException {
         backgroundImage = ImageIO.read(new File(fileName));
     }
@@ -20,7 +17,6 @@ public class JPanelWithBackground extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        // Draw the background image.
         g.drawImage(backgroundImage, 0, 0, this);
     }
 }
