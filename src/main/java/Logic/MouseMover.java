3c312035be1260c4e37ad25ca768dc6d3b271d85
lorenzo <76961588+lolloStorto@users.noreply.github.com>
2023-12-12 09:15:28 +0100

    Update MouseMover.java

--- src/main/java/Logic/MouseMover.java
+++ src/main/java/Logic/MouseMover.java
@@ -3,17 +3,15 @@ import com.github.joonasvali.naturalmouse.api.MouseMotion;
 import com.github.joonasvali.naturalmouse.api.MouseMotionFactory;
 public class MouseMover {
     public static void moveMouseTo(int xDest, int yDest) {
-        // Crea un nuovo thread per eseguire il codice di spostamento del mouse
+
         Thread mouseMoveThread = new Thread(() -> {
             try {
 
-                // Crea un'istanza di MouseMotionFactory
+          
                 MouseMotionFactory mouseMotionFactory = MouseMotionFactory.getDefault();
 
-                // Costruisci un'istanza di MouseMotion per spostare il mouse al punto desiderato
                 MouseMotion motion = mouseMotionFactory.build(xDest, yDest);
-
-                // Sposta il mouse al punto desiderato
+   
                 motion.move();
 
             } catch (InterruptedException e) {
@@ -21,11 +19,9 @@ public class MouseMover {
             }
         });
 
-        // Avvia il nuovo thread
         mouseMoveThread.start();
 
         try {
-            // Attendi fino a quando il thread ha completato l'esecuzione
             mouseMoveThread.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
